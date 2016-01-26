0c3f43268837f814fcddc69ae37054de57cb2274
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-01-26 12:11:24 -0500

    pagination and image file field

--- src/posts/models.py
+++ src/posts/models.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    image = models.FileField(null=True, blank=True)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -20,3 +21,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={"id": self.id})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
