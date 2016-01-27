6ad1206322697dcc78da9b4389c4c64503d4754c
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-01-27 00:51:35 -0500

    added slugs

--- src/posts/views.py
+++ src/posts/views.py
@@ -19,8 +19,8 @@ def post_create(request):
 	}
 	return render(request, "post_form.html", context)
 
-def post_detail(request, id=None):
-	instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
 	context = {
 		"title": instance.title,
 		"instance": instance,
@@ -47,8 +47,8 @@ def post_list(request):
 	return render(request, "post_list.html", context)
 
 
-def post_update(request, id=None):
-	instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -65,8 +65,8 @@ def post_update(request, id=None):
 
 
 
-def post_delete(request, id=None):
-	instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully deleted")
 	return redirect("posts:list")
