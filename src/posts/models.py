6ab35d1a02e279212b707a162dab0dfa947bc51d
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-01-27 01:16:05 -0500

    model manager

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
@@ -14,6 +14,7 @@ def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, 
@@ -38,7 +39,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
-        
+
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
